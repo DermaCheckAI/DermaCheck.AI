4cf7c6d2b2a0b0cff4e66449f9d15e9dac101ccd
tejasvibabar2006 <tejasvibabar2006@gmail.com>
2026-03-16 22:07:31 +0530

    deleted frontend files from backend branch

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="relative z-10 w-full bg-white bg-opacity-5 backdrop-blur-md border-t border-cyan-500 border-opacity-20 py-4 text-center text-gray-400 text-sm">
-      &copy; {new Date().getFullYear()} DermaCheck.AI. Final year Project TYIT Students @AISSMS.
-    </footer>
-  );
-}
