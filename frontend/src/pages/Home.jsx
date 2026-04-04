442bd1ab09579fc138fc6a4e1db29e72654f5237
kartikisable17 <kartikisable22@gmail.com>
2026-04-04 21:33:13 +0530

    fix firebase import issue

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,7 @@ import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
-
+import { signInWithEmailAndPassword, auth } from "../firebase";
 export default function Home() {
 
   const [form, setForm] = useState({
